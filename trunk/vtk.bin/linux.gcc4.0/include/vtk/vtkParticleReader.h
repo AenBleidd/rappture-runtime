74ff73c6b8190f6610b009bee294098ff1714ca5
cxsong <cxsong@6fc65d03-c400-0410-8875-c44a5c2d0901>
2006-05-01 17:46:48 +0000

    added vtk binary (compiled with gcc 4.0) as linux.gcc4.0

---
+++ trunk/vtk.bin/linux.gcc4.0/include/vtk/vtkParticleReader.h
@@ -0,0 +1,89 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    $RCSfile: vtkParticleReader.h,v $
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+// .NAME vtkParticleReader - Read raw particle data and one array.
+// .SECTION Description
+// vtkParticleReader reads a raw file with particles.  It supports
+// random access into the file to read pieces.  The format is:
+// x, y, z, value (all floats).  This class was developed with a 
+// specific file in mind, but may be made more general in the future.
+// Here are a couple of features I am considering:  
+// any data types could be used,
+// arbitrary list of arrays to read (extended to vectors as well),
+// and time serries based on file names.
+
+#ifndef __vtkParticleReader_h
+#define __vtkParticleReader_h
+
+#include "vtkPolyDataSource.h"
+
+#define VTK_FILE_BYTE_ORDER_BIG_ENDIAN 0
+#define VTK_FILE_BYTE_ORDER_LITTLE_ENDIAN 1
+
+class VTK_IO_EXPORT vtkParticleReader : public vtkPolyDataSource
+{
+public:
+  static vtkParticleReader *New();
+  vtkTypeRevisionMacro(vtkParticleReader,vtkPolyDataSource);
+  void PrintSelf(ostream& os, vtkIndent indent);   
+
+  // Description:
+  // Specify file name.
+  vtkSetStringMacro(FileName);
+  vtkGetStringMacro(FileName);
+
+  // Description:
+  // These methods should be used instead of the SwapBytes methods.
+  // They indicate the byte ordering of the file you are trying
+  // to read in. These methods will then either swap or not swap
+  // the bytes depending on the byte ordering of the machine it is
+  // being run on. For example, reading in a BigEndian file on a
+  // BigEndian machine will result in no swapping. Trying to read
+  // the same file on a LittleEndian machine will result in swapping.
+  // As a quick note most UNIX machines are BigEndian while PC's
+  // and VAX tend to be LittleEndian. So if the file you are reading
+  // in was generated on a VAX or PC, SetDataByteOrderToLittleEndian 
+  // otherwise SetDataByteOrderToBigEndian. 
+  void SetDataByteOrderToBigEndian();
+  void SetDataByteOrderToLittleEndian();
+  int GetDataByteOrder();
+  void SetDataByteOrder(int);
+  const char *GetDataByteOrderAsString();
+
+  // Description:
+  // Set/Get the byte swapping to explicitly swap the bytes of a file.
+  vtkSetMacro(SwapBytes,int);
+  int GetSwapBytes() {return this->SwapBytes;}
+  vtkBooleanMacro(SwapBytes,int);
+
+protected:
+  vtkParticleReader();
+  ~vtkParticleReader();
+
+  void OpenFile();
+
+  char *FileName;
+  ifstream *File;
+  int SwapBytes;
+
+  unsigned long NumberOfPoints;
+  
+  void ExecuteInformation();
+  void Execute();
+private:
+  vtkParticleReader(const vtkParticleReader&);  // Not implemented.
+  void operator=(const vtkParticleReader&);  // Not implemented.
+};
+
+#endif
---
+++ trunk/vtk.bin/linux.gcc4.0/include/vtk/vtkSmartPointerBase.h
@@ -0,0 +1,111 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    $RCSfile: vtkSmartPointerBase.h,v $
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+// .NAME vtkSmartPointerBase - Non-templated superclass for vtkSmartPointer.
+// .SECTION Description
+// vtkSmartPointerBase holds a pointer to a vtkObjectBase or subclass
+// instance and performs one Register/UnRegister pair.  This is useful
+// for storing VTK objects in STL containers.  This class is not
+// intended to be used directly.  Instead, use the vtkSmartPointer
+// class template to automatically perform proper cast operations.
+
+#ifndef __vtkSmartPointerBase_h
+#define __vtkSmartPointerBase_h
+
+#include "vtkObjectBase.h"
+
+class VTK_COMMON_EXPORT vtkSmartPointerBase
+{
+public:
+  // Description:
+  // Initialize smart pointer to NULL.
+  vtkSmartPointerBase();
+  
+  // Description:
+  // Initialize smart pointer to given object.
+  vtkSmartPointerBase(vtkObjectBase* r);
+  
+  // Description:
+  // Initialize smart pointer with a new reference to the same object
+  // referenced by given smart pointer.
+  vtkSmartPointerBase(const vtkSmartPointerBase& r);
+  
+  // Description:
+  // Destroy smart pointer and remove the reference to its object.
+  ~vtkSmartPointerBase();
+  
+  // Description:
+  // Assign object to reference.  This removes any reference to an old
+  // object.
+  vtkSmartPointerBase& operator=(vtkObjectBase* r);
+  vtkSmartPointerBase& operator=(const vtkSmartPointerBase& r);
+  
+  // Description:
+  // Get the contained pointer.
+  vtkObjectBase* GetPointer() const
+    {
+    // Inline implementation so smart pointer comparisons can be fully
+    // inlined.
+    return this->Object;
+    }
+protected:
+  
+  // Internal utility methods.
+  void Swap(vtkSmartPointerBase& r);
+  void Register();
+  void UnRegister();
+  
+  // Pointer to the actual object.
+  vtkObjectBase* Object;
+};
+
+//----------------------------------------------------------------------------
+// Need to use vtkstd_bool type because std::less requires bool return
+// type from operators.  This example should not be used to justify
+// using bool elsewhere in VTK.
+
+#define VTK_SMART_POINTER_BASE_DEFINE_OPERATOR(op) \
+  inline vtkstd_bool \
+  operator op (const vtkSmartPointerBase& l, const vtkSmartPointerBase& r) \
+    { \
+    return (static_cast<void*>(l.GetPointer()) op \
+            static_cast<void*>(r.GetPointer())); \
+    } \
+  inline vtkstd_bool \
+  operator op (vtkObjectBase* l, const vtkSmartPointerBase& r) \
+    { \
+    return (static_cast<void*>(l) op static_cast<void*>(r.GetPointer())); \
+    } \
+  inline vtkstd_bool \
+  operator op (const vtkSmartPointerBase& l, vtkObjectBase* r) \
+    { \
+    return (static_cast<void*>(l.GetPointer()) op static_cast<void*>(r)); \
+    }
+// Description:
+// Compare smart pointer values.
+VTK_SMART_POINTER_BASE_DEFINE_OPERATOR(==)  
+VTK_SMART_POINTER_BASE_DEFINE_OPERATOR(!=)
+VTK_SMART_POINTER_BASE_DEFINE_OPERATOR(<)
+VTK_SMART_POINTER_BASE_DEFINE_OPERATOR(<=)
+VTK_SMART_POINTER_BASE_DEFINE_OPERATOR(>)
+VTK_SMART_POINTER_BASE_DEFINE_OPERATOR(>=)
+
+#undef VTK_SMART_POINTER_BASE_DEFINE_OPERATOR
+
+// Description:
+// Streaming operator to print smart pointer like regular pointers.
+VTK_COMMON_EXPORT ostream& operator << (ostream& os,
+                                        const vtkSmartPointerBase& p);
+
+#endif
