7376d62ed39c633708c2b58f64212e2409b7990c
dkearney <dkearney@6fc65d03-c400-0410-8875-c44a5c2d0901>
2008-04-24 22:11:24 +0000

    removing old vtk binaries, we now build from source. the cron.* files are also old and unused.

--- vtk.bin/mac/lib/vtk/tcl/vtkinteraction/vtkinteraction.tcl
+++
@@ -1,7 +0,0 @@
-package require -exact vtkrendering 4.4
-
-foreach s {Interactor bindings-rw bindings-iw bindings setget} {
-  source [file join [file dirname [info script]] "${s}.tcl"]
-}
-
-package provide vtkinteraction 4.4
